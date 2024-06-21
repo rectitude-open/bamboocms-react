6996a05635dcefd43e61edfe976b4b308e68c53c
aspirantzhang <admin@aspirantzhang.com>
2024-06-21 08:58:37 +0800

    feat: add react query and dev tools

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,18 @@
+'use client';
+
 import * as React from 'react';
 import type { Viewport } from 'next';
 
 import '@/styles/global.css';
 
-import { UserProvider } from '../contexts/user-context';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import { LocalizationProvider } from '../components/core/localization-provider';
 import { ThemeProvider } from '../components/core/theme-provider/theme-provider';
+import { UserProvider } from '../contexts/user-context';
+
+const queryClient = new QueryClient();
 
 export const viewport = { width: 'device-width', initialScale: 1 } satisfies Viewport;
 
@@ -17,11 +24,14 @@ export default function Layout({ children }: LayoutProps): React.JSX.Element {
   return (
     <html lang="en">
       <body>
-        <LocalizationProvider>
-          <UserProvider>
-            <ThemeProvider>{children}</ThemeProvider>
-          </UserProvider>
-        </LocalizationProvider>
+        <QueryClientProvider client={queryClient}>
+          <ReactQueryDevtools initialIsOpen={false} />
+          <LocalizationProvider>
+            <UserProvider>
+              <ThemeProvider>{children}</ThemeProvider>
+            </UserProvider>
+          </LocalizationProvider>
+        </QueryClientProvider>
       </body>
     </html>
   );
