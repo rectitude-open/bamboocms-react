e58d7f81e2afc5368ff003c2d909a2ba9eccc778
aspirantzhang <admin@aspirantzhang.com>
2024-07-17 11:45:59 +0800

    refactor: use next lint rule

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const config = {
   output: 'export',
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
 };
 
 export default config;
