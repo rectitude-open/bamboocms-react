5a35efb98cc50d997874bf9142b0172d577c141d
aspirantzhang <admin@aspirantzhang.com>
2024-07-17 15:19:11 +0800

    feat: add global ApiResponse type

--- types/api.d.ts
+++ types/api.d.ts
@@ -1,3 +1,13 @@
 import type { AxiosRequestConfig } from 'axios';
 
+export interface ApiResponse {
+  success: boolean;
+  data: Record<string, unknown> | Record<string, unknown>[];
+  meta?: {
+    total: number;
+    current_page: number;
+    per_page: number;
+  };
+}
+
 export type FetchFunction = (params: Record<string, unknown>, config?: AxiosRequestConfig) => Promise<any>;
