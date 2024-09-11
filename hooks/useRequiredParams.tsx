aa1d2bd3e3bb6c33c4e7d60f5cb71bcad836de6a
aspirantzhang <admin@aspirantzhang.com>
2024-09-11 13:31:48 +0800

    refactor: improve useRequiredParams to support row data

--- hooks/useRequiredParams.tsx
+++ hooks/useRequiredParams.tsx
@@ -2,13 +2,26 @@ import React, { useEffect, useMemo, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { Alert } from '@mui/material';
 
-const useRequiredParams = (requiredParams: string[]) => {
+interface UseRequiredParamsProps<T> {
+  requiredParams: string[];
+  row?: T;
+}
+
+const useRequiredParams = <T extends Record<string, unknown>>({
+  requiredParams,
+  row = {} as T,
+}: UseRequiredParamsProps<T>) => {
   const [missingParams, setMissingParams] = useState<string[]>([]);
   const searchParams = useSearchParams();
+  const searchParamsString = searchParams.toString();
 
   const requiredParamsMap: Record<string, string | null> = useMemo(
-    () => requiredParams.reduce((acc, param) => ({ ...acc, [param]: searchParams.get(param) }), {}),
-    [searchParams]
+    () =>
+      requiredParams.reduce((acc, param) => {
+        const paramValue = Object.keys(row).length ? row[param] : searchParams.get(param);
+        return { ...acc, [param]: paramValue };
+      }, {}),
+    [searchParamsString, row, requiredParams]
   );
 
   useEffect(() => {
