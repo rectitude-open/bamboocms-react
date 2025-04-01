9bd50b632648a4c7af5c042bb8353cfd9112ba90
aspirantzhang <admin@aspirantzhang.com>
2025-04-01 20:41:52 +0800

    feat: add CustomToolbarActions component to enhance dashboard layout

---
+++ app/(dashboard)/CustomToolbarActions.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import { ToolbarActions } from '@toolpad/core/DashboardLayout';
+
+const CustomToolbarActions = () => {
+  return (
+    <>
+      <ToolbarActions />
+      <>Account</>
+    </>
+  );
+};
+
+export default CustomToolbarActions;
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,15 @@
-import { DashboardLayout } from '@toolpad/core/DashboardLayout';
+import { DashboardLayout, ToolbarActions } from '@toolpad/core/DashboardLayout';
 import { PageContainer } from '@toolpad/core/PageContainer';
 import * as React from 'react';
 
+import CustomToolbarActions from './CustomToolbarActions';
+
 export default function Layout(props: { children: React.ReactNode }) {
   return (
-    <DashboardLayout>
+    <DashboardLayout
+      slots={{
+        toolbarActions: CustomToolbarActions,
+      }}>
       <PageContainer>{props.children}</PageContainer>
     </DashboardLayout>
   );
